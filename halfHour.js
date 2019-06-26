104d7e07241dc83825f8f6e7edba83309e1b7449
kylepadernilla <47429198+kylepadernilla@users.noreply.github.com>
2019-06-26 15:32:40 -0400

    Added half-hour js file.

---
+++ halfHour.js
@@ -0,0 +1,60 @@
+//Vanilla javascript that runs a function at a particular time period for 5 minutes by the hour.
+//Example: Shows an image for 5 minutes every 30 minutes by the hour(9:30, 10:30, 11:30, etc.).
+
+var minsMin = 30;
+var minsMax = 35;
+
+function printDoc1(){
+
+//randomizer of quotes.
+  return "<img class=\"hero\" src=images/myhero.jpg> </img>";
+
+}
+function checkTime() {
+  var time = new Date();
+  var mins = time.getMinutes();
+  var hours= time.getHours();
+  //every 30 minutes?
+  //mins = (59-mins) % 30;
+
+  var secs = time.getSeconds();
+
+//This is a countdown script for seconds. Really useful.
+/*
+  if (secs != 60)
+  { secs = (59-secs) % 60; }
+  else
+  { secs = 00; }
+
+*/
+
+//Adds a 0 in the front of the seconds when it runs.
+secs = (secs < 10) ? "0" + secs : secs;
+
+if(hours > 12){
+        hours = hours - 12;
+    }
+
+  //get time and timeformat.
+  //time = [Number(mins),Number(secs)];
+  timeFormat= hours + ":" + mins + ":" + secs;
+
+//Condition: Between H:30 to H:35 in normal clock time.
+  if (mins>=minsMin && mins<minsMax  && secs >= 0){
+      return "<h2>" + timeFormat + "</h2>" +printDoc1();
+  }
+else{
+  return "<h2>" + timeFormat + "</h2>";
+}
+}
+
+//Example of Continuous Updating.
+var myVar= setInterval(printDoc, 1000);
+
+printDoc();
+
+function printDoc(){
+
+//put what is in body.
+document.querySelector(".section .second").innerHTML= checkTime();
+}
