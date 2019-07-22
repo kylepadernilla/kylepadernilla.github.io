47dd774b7d319a81e48d5a06ce52e301edc75bd0
kylepadernilla <47429198+kylepadernilla@users.noreply.github.com>
2019-07-22 16:41:10 -0400

    Added Javascript to Accordion Component

---
+++ blogscript.js
@@ -0,0 +1,22 @@
+const expandButton= document.querySelector(".btn"); //DataButton
+const operatorSymbol= document.querySelector(".header2 p");
+
+var boolean= false;
+
+function clickedIt(boolean) {
+   var canSee = expandButton.classList.contains("collapsed"); //vanilla javascript of hasclass in jquery. Most effective way to check class names.
+   boolean = canSee;
+   return boolean;
+}
+
+function expand() {
+var buttonClicked= clickedIt(boolean);
+
+if(buttonClicked == true)
+{
+  operatorSymbol.innerHTML = "-";
+}
+else{ operatorSymbol.innerHTML= "+"}
+}
+
+expandButton.addEventListener("click",expand,false);
